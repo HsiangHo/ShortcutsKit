e308319840a79cfd987c8afbc3856d6bfabb08ca
HsiangHo <ho_cheung@qq.com>
2018-10-09 21:13:54 +0800

    Initialize

---
+++ ShortcutsKit/ShortcutsDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ShortcutsDemo
+//
+//  Created by Jovi on 10/9/18.
+//  Copyright © 2018 Jovi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
---
+++ ShortcutsKit/ShortcutsKit/ShortcutsKit.h
@@ -0,0 +1,19 @@
+//
+//  ShortcutsKit.h
+//  ShortcutsKit
+//
+//  Created by Jovi on 10/9/18.
+//  Copyright © 2018 Jovi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ShortcutsKit.
+FOUNDATION_EXPORT double ShortcutsKitVersionNumber;
+
+//! Project version string for ShortcutsKit.
+FOUNDATION_EXPORT const unsigned char ShortcutsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShortcutsKit/PublicHeader.h>
+
+
