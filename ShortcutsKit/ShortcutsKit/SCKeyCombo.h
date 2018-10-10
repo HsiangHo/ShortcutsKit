87320ca8574139629af5d7dee6e8304ce1bd00f1
HsiangHo <ho_cheung@qq.com>
2018-10-10 21:18:18 +0800

    implement SCKeyCombo

--- ShortcutsKit/ShortcutsKit/SCKeyCombo.h
+++ ShortcutsKit/ShortcutsKit/SCKeyCombo.h
@@ -8,7 +8,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Carbon/Carbon.h>
 
-@interface SCKeyCombo : NSObject
+NS_ASSUME_NONNULL_BEGIN
+@interface SCKeyCombo : NSObject <NSCoding>
+
+@property (nonatomic, assign)               UInt32          keyCode;
+@property (nonatomic, assign)               UInt32          keyModifiers;
+@property (nonatomic, copy, readonly)       NSString        *stringForKeyCombo;
+
+-(instancetype)initWithKeyCode:(UInt32)keyCode keyModifiers:(UInt32)modifiers;
+
+
+//Convert keycode to string.  such as 'kVK_ANSI_F' to 'f', 'kVK_ANSI_F' + 'shiftKey' to 'F'
++(nullable NSString *)keyCode2String:(UInt32)keyCode keyModifiers:(UInt32)modifiers;
+
+//Convert keyModifiers to string.  'shiftKey' to '⇧'
++(nullable NSString *)keyModifiers2String:(UInt32)modifiers;
 
 @end
+NS_ASSUME_NONNULL_END
