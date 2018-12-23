dd86acef681f8717f10a0ab5e89dda52265a29b5
HsiangHo <ho_cheung@qq.com>
2018-12-24 00:04:26 +0800

    update demo

--- ShortcutsKit/ShortcutsDemo/AppDelegate.m
+++ ShortcutsKit/ShortcutsDemo/AppDelegate.m
@@ -18,10 +18,53 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    
+    //Create a keyCombo object for "shift + option + control + space"
     SCKeyCombo *keyCombo = [[SCKeyCombo alloc] initWithKeyCode:kVK_Space keyModifiers:shiftKey + optionKey + controlKey];
+    
+    //Serialize keyCombo object
+    [[NSUserDefaults standardUserDefaults] setValue:[NSKeyedArchiver archivedDataWithRootObject:keyCombo] forKey:@"keyCombo"];
+    
+    //Deserialization keyCombo object
+    SCKeyCombo *keyCombo2 = [NSKeyedUnarchiver unarchiveObjectWithData:[[NSUserDefaults standardUserDefaults] valueForKey:@"keyCombo"]];
+    NSLog(@"%@",[keyCombo2 stringForKeyCombo]);
+    
+    //Create a keyComboView to show the keyCombo
     SCKeyComboView *keyComboView = [SCKeyComboView standardKeyComboView];
+    //Cunstomize keyComboView
+    [keyComboView setOnTintColor:[NSColor redColor]];
+    //Set delegate to handle keyconbo changed notification
     [keyComboView setDelegate:(id<SCKeyComboViewDelegate>)self];
-    [keyComboView setKeyCombo:keyCombo];
+    [keyComboView setKeyCombo:keyCombo2];
+    
+    //Create a hotkey object from a keyCombo object
+    //Using block
+    SCHotkey *shortcut = [[SCHotkey alloc] initWithKeyCombo:keyCombo2 identifier:@"shortcut" handler:^(SCHotkey * _Nonnull hotkey) {
+        NSLog(@"shortcut has been called");
+    }];
+    [shortcut register];
+    
+    //Using selector
+    SCKeyCombo *keyCombo3 = [[SCKeyCombo alloc] initWithKeyCode: kVK_ANSI_B keyModifiers: optionKey + controlKey];
+    SCHotkey *shortcut2 = [[SCHotkey alloc]initWithKeyCombo:keyCombo3 identifier:@"shortcut2" target:self action:@selector(shortcut2Callback)];
+    [shortcut2 register];
+    
+    //Invoke hotkey object action
+    [shortcut invoke];
+    
+    //Unregister hotkey object
+    [shortcut unregister];
+    
+    //About SCHotkeyManager
+    //Register with hotkey object
+    [[SCHotkeyManager sharedManager] registerWithHotkey:shortcut];
+    //Unregister with identifier
+    [[SCHotkeyManager sharedManager] unregisterWithIdentifier:@"shortcut"];
+    //Unregister with hotkey object
+    [[SCHotkeyManager sharedManager] unregisterWithHotkey:shortcut2];
+    //Unregister all hotkey objects
+    [[SCHotkeyManager sharedManager] unregisterAllHotkeys];
+    
     [[_window contentView] addSubview:keyComboView];
     [keyComboView setFrameOrigin:NSMakePoint(100, 118)];
 }
@@ -29,6 +72,10 @@
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+}
+
+-(void)shortcut2Callback{
+    NSLog(@"shortcut2 has been called");
 }
 
 -(void)keyComboWillChange:(SCKeyComboView *)keyComboView{
